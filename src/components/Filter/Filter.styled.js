8170af2b93900d26b102de42b10db5fafe0e7e4e
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2022-01-05 14:07:13 +0200

    ...

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -9,7 +9,7 @@ export const TriggerButton = styled.button`
   align-items: center;
   margin: 0;
   padding: 8px 12px;
-  background-color: yellow;
+  background-color: red;
   width: 150px;
   height: 40px;
   border-radius: 5px;
